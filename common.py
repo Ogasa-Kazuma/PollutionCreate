d268c6ce80c5116371738430e697ed3f000a30b6
Ogasa Kazuma <knct0831@gmail.com>
2022-01-06 04:59:40 +0900

    remove: common.py

--- common.py
+++
@@ -1,7 +0,0 @@
-import math
-
-def CalculateAbsoluteDistance(xBegin, yBegin, xEnd, yEnd):
-
-    diff = math.sqrt((xEnd - xBegin) * (xEnd - xBegin) + (yEnd- yBegin) * (yEnd - yBegin))
-
-    return  diff
